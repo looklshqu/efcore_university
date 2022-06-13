acd83b8ee99b418fd8203b6b68240fa92b7c9ed0
looklsh <looklsh38@gmail.com>
2022-06-13 10:41:35 +0900

    sort-filter-paging

--- ContosoUniversity/Controllers/HomeController.cs
+++ ContosoUniversity/Controllers/HomeController.cs
@@ -1,5 +1,8 @@
-﻿using ContosoUniversity.Models;
+﻿using ContosoUniversity.Data;
+using ContosoUniversity.Models;
+using ContosoUniversity.Models.SchoolViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace ContosoUniversity.Controllers
@@ -7,10 +10,12 @@ namespace ContosoUniversity.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly SchoolContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, SchoolContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
@@ -21,6 +26,20 @@ namespace ContosoUniversity.Controllers
         public IActionResult Privacy()
         {
             return View();
+        }
+
+        public async Task<IActionResult> About()
+        {
+            IQueryable<EntrollmentDateGroup> data =
+                from student in _context.Students
+                group student by student.EnrollmentDate into dateGroup
+                select new EntrollmentDateGroup()
+                {
+                    EntrollmentDate = dateGroup.Key,
+                    StudentCount = dateGroup.Count(),
+                };
+
+            return View(await data.AsNoTracking().ToListAsync());
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ ContosoUniversity/Models/SchoolViewModels/EntrollmentDateGroup.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ContosoUniversity.Models.SchoolViewModels
+{
+    public class EntrollmentDateGroup
+    {
+        [DataType(DataType.Date)]
+        public DateTime? EntrollmentDate { get; set; }
+        public int StudentCount { get; set; }
+    }
+}
